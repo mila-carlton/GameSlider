820300ffe7d72c2b7d630a9127a6abb68c89a01d
mila-carlton <mila.carlton@mail.ru>
2023-12-25 00:17:10 +0400

    new

---
+++ GameSlider/SliderRepresentative.swift
@@ -0,0 +1,18 @@
+//
+//  SliderRepresentative.swift
+//  GameSlider
+//
+//  Created by PASGON TEXTILE on 24.12.23.
+//
+
+import SwiftUI
+
+struct SliderRepresentative: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SliderRepresentative()
+}
