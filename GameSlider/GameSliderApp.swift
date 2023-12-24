19b9552eac7a12035ec5c350892a7b8f37fec39d
mila-carlton <mila.carlton@mail.ru>
2023-12-24 15:55:22 +0400

    Initial Commit

---
+++ GameSlider/GameSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameSliderApp.swift
+//  GameSlider
+//
+//  Created by PASGON TEXTILE on 24.12.23.
+//
+
+import SwiftUI
+
+@main
+struct GameSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
