78a4456d33732eacb4db6b9fce9bad1d02f3bee3
mila-carlton <mila.carlton@mail.ru>
2023-12-25 00:19:07 +0400

    news

--- GameSlider/ContentView.swift
+++ GameSlider/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var targetValue = Int.random(in: 1...100)
+    @State private var currentValue = 50.0
+    @State private var showAlert = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Подвинь меня поближе к: \(targetValue)")
+                .padding()
+            SliderRepresentative(value: $currentValue, targetValue: targetValue)
+                .padding()
+            VStack {
+                Button("Начать заново") {
+                    targetValue = Int.random(in: 1...100)
+                    currentValue = 50.0
+                }
+                .padding()
+                Button("Проверь меня") {
+                    showAlert = true
+                }
+                .padding()
+                .alert(isPresented: $showAlert) {
+                    let score = computeScore()
+                    return Alert(title: Text("Ваш результат: \(score)"))
+                }
+            }
+            
         }
-        .padding()
+    }
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - lround(currentValue))
+        return 100 - difference
     }
 }
 
--- GameSlider/SliderRepresentative.swift
+++ GameSlider/SliderRepresentative.swift
@@ -7,12 +7,49 @@
 
 import SwiftUI
 
-struct SliderRepresentative: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct SliderRepresentative: UIViewRepresentable {
+    
+    @Binding var value: Double
+    var targetValue: Int
+    
+    func makeUIView(context: Context) -> UISlider {
+        let slider = UISlider()
+        slider.maximumValue = 1
+        slider.maximumValue = 100
+        slider.value = Float(value)
+        
+        slider.addTarget(
+            context.coordinator,
+                         
+            action: #selector(Coordinator.valueChanged(_:)),
+            for: .valueChanged)
+        return slider
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        uiView.value = Float(value)
+        let difference = abs(targetValue - lround(value))
+        let alpha = CGFloat(100 - difference) / 100
+        uiView.thumbTintColor = UIColor.red.withAlphaComponent(alpha)
+}
+    func makeCoordinator() -> Coordinator {
+        Coordinator(value: $value)
+    }
+    }
+extension SliderRepresentative {
+    class Coordinator: NSObject {
+        @Binding var value: Double
+        init(value: Binding<Double>) {
+            self._value = value
+        }
+        @objc
+        func valueChanged(_ sender: UISlider) {
+            value = Double(sender.value)
+        }
+        
     }
 }
 
 #Preview {
-    SliderRepresentative()
+    SliderRepresentative(value: .constant(80), targetValue: 50)
 }
